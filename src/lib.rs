e3c68429e5e982b468da1dd7f9b1f914b62ae2b3
Joshua Rully <joshrully@gmail.com>
2024-08-10 21:02:18 -0700

    [main] Fix fmt error

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use actix_web::{dev::Server, HttpResponse, HttpServer, web, App};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 use serde::Deserialize;
 use std::net::TcpListener;
 
